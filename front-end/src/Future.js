9ea98c0e26da020a807601d66ebbbc8c206fc021
DSuh <danielsuh20@gmail.com>
2020-03-25 16:47:33 -0700

    Issue #27: Added link to signup page and fixed styling to be more responsive

--- front-end/src/Future.js
+++ front-end/src/Future.js
@@ -13,14 +13,14 @@ const FutureOverview = ( props ) => {
   )
 }
 
-const FutureDiagram = ( props ) => {
-  return (
-    <futureDiagramBox>
-      /*<h1 className="overViewH1">{props.title}</h1>
-      <p className="overViewP">{props.text}</p>*/
-    </futureDiagramBox>
-  )
-}
+// const FutureDiagram = ( props ) => {
+//   return (
+//     <futureDiagramBox>
+//       /*<h1 className="overViewH1">{props.title}</h1>
+//       <p className="overViewP">{props.text}</p>*/
+//     </futureDiagramBox>
+//   )
+// }
 
 const FutureSummary = ( props ) => {
   return (
@@ -41,58 +41,60 @@ const Future = () => {
         </futureOverall>
         <futureGraph>
           <futureDiagramBox>
-            <Chart
-              width={'60%'}
-              height={'300px'}
-              chartType="PieChart"
-              loader={<div>Loading Chart</div>}
-              data={[
-                ['Expense', 'Dollars'],
-                ['Food', 2000],
-                ['Rent', 4000],
-                ['Commute', 2500],
-                ['Entertainment', 1500],
-                ['Utilities', 350],
-              ]}
-              options={{
-                chartArea: {left: '5%', right: '5%',},
-                //backgroundColor: 'red',
-                title: 'Overall Expenses',
-                // Just add this option
-                is3D: true,
-              }}
-              rootProps={{ 'data-testid': '2' }}
-            />
-            <Chart
-              width={'100%'}
-              height={'300px'}
-              chartType="ColumnChart"
-              loader={<div>Loading Chart</div>}
-              data={[
-                ['Month', 'In', 'Out'],
-                ['Jan', 10000, 8000],
-                ['Feb', 10000, 4000],
-                ['Mar', 10000, 11000],
-                ['Apr', 10000, 3000],
-                ['May', 10000, 5000],
-                ['June', 10000, 8000],
-                ['July', 10000, 4650],
-                ['Aug', 10000, 6000],
-                ['Sept', 10000, 3470],
-                ['Oct', 10000, 2340],
-                ['Nov', 10000, 5232],
-                ['Dec', 10000, 7234],
-              ]}
-              options={{
-                chartArea: {left: '10%', right: '12%',},
-                chart: {title: 'Income In vs. Out'},
-                vAxis: { title: 'Money', },
-                hAxis: { title: 'Month' ,},
-                //backgroundColor: 'yellow',
-              }}
-              // For tests
-              rootProps={{ 'data-testid': '3' }}
-            />
+              <Chart
+                width={'100%'}
+                height={'300px'}
+                chartType="PieChart"
+                loader={<div>Loading Chart</div>}
+                data={[
+                  ['Expense', 'Dollars'],
+                  ['Food', 2000],
+                  ['Rent', 4000],
+                  ['Commute', 2500],
+                  ['Entertainment', 1500],
+                  ['Utilities', 350],
+                ]}
+                options={{
+                  chartArea: {left: '5%', right: '5%',},
+                  //backgroundColor: 'red',
+                  title: 'Overall Expenses',
+                  // Just add this option
+                  is3D: true,
+                }}
+                rootProps={{ 'data-testid': '2' }}
+              />
+            </futureDiagramBox>
+            <futureDiagramBox>
+              <Chart
+                width={'600px'}
+                height={'300px'}
+                chartType="ColumnChart"
+                loader={<div>Loading Chart</div>}
+                data={[
+                  ['Month', 'In', 'Out'],
+                  ['Jan', 10000, 8000],
+                  ['Feb', 10000, 4000],
+                  ['Mar', 10000, 11000],
+                  ['Apr', 10000, 3000],
+                  ['May', 10000, 5000],
+                  ['June', 10000, 8000],
+                  ['July', 10000, 4650],
+                  ['Aug', 10000, 6000],
+                  ['Sept', 10000, 3470],
+                  ['Oct', 10000, 2340],
+                  ['Nov', 10000, 5232],
+                  ['Dec', 10000, 7234],
+                ]}
+                options={{
+                  chartArea: {left: '10%', right: '12%',},
+                  chart: {title: 'Income In vs. Out'},
+                  vAxis: { title: 'Money', },
+                  hAxis: { title: 'Month' ,},
+                  //backgroundColor: 'yellow',
+                }}
+                // For tests
+                rootProps={{ 'data-testid': '3' }}
+              />
           </futureDiagramBox>
         </futureGraph>
         <futureSummary>
@@ -116,12 +118,12 @@ const Future = () => {
                 </form>
                 </div>
                 <div className="button-actions">
-                  <button className="button" onClick={() => {
+                  <button className="future-save-button" onClick={() => {
                       console.log("modal closed ");
                       close();
-                    }}
-                  >
-                    Save
+                      window.location.href="/sign-up";
+                    }}>
+                    Sign in to Save Your Future
                   </button>
                 </div>
               </div>
