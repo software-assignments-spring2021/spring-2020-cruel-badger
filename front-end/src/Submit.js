5ee55b7f5f997e3100b77e572395a6f5e8194fe7
Gauresh Walia <gaureshwalia@gmail.com>
2020-05-06 05:31:29 -0400

    got signup and create future form validation

--- front-end/src/Submit.js
+++ front-end/src/Submit.js
@@ -9,6 +9,8 @@ function Submit(props) {
 		<div>
 			<h2 className="stepTitle">Submit</h2>
 			<button href="" className="submitButton" onClick={props.handleSubmit}>Submit Results!</button>
+			<p>If submission doesn't work, it means there is some type of issue with your input</p>
+			<p>Go check your entries to make sure nothing is missing and everything is filled properly</p>
 		</div>
 	);
 
